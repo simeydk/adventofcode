aa1e3a493e75571af0734cd5ca18f0c74d265558
Simey de Klerk <simeydeklerk@gmail.com>
2021-12-18 13:03:44 +0200

    day 16 part 2

--- 2021/day16.py
+++ 2021/day16.py
@@ -2,10 +2,11 @@ from typing import List
 from io import StringIO
 from dataclasses import dataclass, field
 from inspect import signature
+from math import prod
 
 DAY = 16
 TEST_SOLUTION_1 = 12
-TEST_SOLUTION_2 = None
+TEST_SOLUTION_2 = 46
 
 @dataclass
 class Packet:
@@ -13,6 +14,8 @@ class Packet:
     type: int
 
     version_sum: int = field(init=False, default = 0, repr = False, compare = False)
+
+    value: int = field(init=False, default = 0, repr = False, compare = False)
 
 @dataclass 
 class Literal(Packet):
@@ -27,8 +30,29 @@ class Operator(Packet):
     subpackets: List[Packet]
 
     @property
-    def version_sum(self):
+    def version_sum(self) -> int:
         return self.version + sum(packet.version_sum for packet in self.subpackets)
+
+    @property
+    def value(self) -> int:
+        sub_values = [packet.value for packet in self.subpackets]
+        if self.type == 0:
+            return sum(sub_values)
+        elif self.type == 1:
+            return prod(sub_values)
+        elif self.type == 2:
+            return min(sub_values)
+        elif self.type == 3:
+            return max(sub_values)
+        elif self.type == 5:
+            return (sub_values[0] > sub_values[1]) * 1
+        elif self.type == 6:
+            return (sub_values[0] < sub_values[1]) * 1
+        elif self.type == 7:
+            return (sub_values[0] == sub_values[1]) * 1
+        else:
+            raise Exception(f"Unknown operator type: {self.type}")
+        
 
 
 def hex_to_bin(hex_str):
@@ -74,7 +98,8 @@ def part1(data: str) -> int:
 
 
 def part2(data: str) -> int:
-    pass
+    stream = parse_input(data)
+    return parse_packet(stream).value
 
 test_input = "620080001611562C8802118E34"
 
@@ -82,6 +107,16 @@ assert part1("8A004A801A8002F478") == 16
 assert part1("620080001611562C8802118E34") == 12
 assert part1("C0015000016115A2E0802F182340") == 23
 assert part1("A0016C880162017C3686B18A3D4780") == 31
+
+
+assert part2("C200B40A82") == 3
+assert part2("04005AC33890") == 54
+assert part2("880086C3E88112") == 7
+assert part2("CE00C43D881120") == 9
+assert part2("D8005AC2A8F0") == 1
+assert part2("F600BC2D8F") == 0
+assert part2("9C005AC2F8F0") == 0
+assert part2("9C0141080250320F1802104A08") == 1
 
 input_raw = read_file(f'2021/data/day{DAY:02d}/input.txt')
 
