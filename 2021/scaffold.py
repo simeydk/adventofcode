961ed97f38783ea428d0d23cedab95e83649d5a7
Simey de Klerk <simeydeklerk@gmail.com>
2021-12-18 08:23:33 +0200

    update scaffold

--- 2021/scaffold.py
+++ 2021/scaffold.py
@@ -1,34 +1,32 @@
 from typing import List
 
-def read_file(filename):
-    with open(filename) as f:
-        return [line.strip() for line in f.readlines()]
-
-def part1(data: List[str]) -> int:
-    pass
-
-def part2(data: List[str]) -> int:
-    pass
-
-test_input = [
-    '#PASTE HERE'
-]
-
-
 DAY = 11
 TEST_SOLUTION_1 = None
 TEST_SOLUTION_2 = None
+
+def read_file(filename) -> str:
+    with open(filename, encoding="UTF-8") as f:
+        return f.read()
+
+def part1(data: str) -> int:
+    pass
+
+def part2(data: str) -> int:
+    pass
+
+test_input = """Paste here"""
+
 
 input_raw = read_file(f'2021/data/day{DAY:02d}/input.txt')
 
 if TEST_SOLUTION_1:
     assert part1(test_input) == TEST_SOLUTION_1
-    print(f"Solution 1: {part1(input_raw)}")
+    print(f"Solution 1:\n{part1(input_raw)}")
     if TEST_SOLUTION_2:
         assert part2(test_input) == TEST_SOLUTION_2
-        print(f"Solution 2: {part2(input_raw)}")
+        print(f"Solution 2:\n{part2(input_raw)}")
     else:
-        print(f"Test 2: {part2(test_input)}")
+        print(f"Test 2:\n{part2(test_input)}")
 else:
-    print(f"Test 1: {part1(test_input)}")
+    print(f"Test 1:\n{part1(test_input)}")
     
