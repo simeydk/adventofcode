9739f03d7684ebf54192a3111499ce4d14855e48
Simey de Klerk <simeydeklerk@gmail.com>
2021-12-21 07:49:05 +0200

    day20 WIP

--- 2021/day20.py
+++ 2021/day20.py
@@ -2,13 +2,6 @@ from typing import DefaultDict, Dict, List
 import numpy as np
 from collections import defaultdict
 
-d = defaultdict(lambda: 0)
-
-d[1] = 1
-d[2] = 2
-print(d[7])
-print(d.items())
-
 DAY = 20
 TEST_SOLUTION_1 = None
 TEST_SOLUTION_2 = None
@@ -23,52 +16,46 @@ def hashperiod_to_binarray(s: str) -> List[int]:
 def parse_input(data: str):
     key_raw, pic_raw = data.split('\n\n')
     key = hashperiod_to_binarray(key_raw)
-    pic_list = [hashperiod_to_binarray(line) for line in pic_raw.splitlines()]
-    pic_dict = defaultdict(lambda: 0)
-    for i in range(len(pic_list)):
-        for j in range(len(pic_list[0])):
-            pic_dict[(i,j)] = pic_list[i][j]
-    return key, pic_dict
+    pic = np.array([hashperiod_to_binarray(line) for line in pic_raw.splitlines()])
+    return key, pic
 
 def append_zeros(mx: np.ndarray, n: int = 1) -> np.ndarray:
     mx = np.concatenate((np.zeros([mx.shape[0], n], dtype=np.int8), mx, np.zeros([mx.shape[0], n], dtype=np.int8)), axis = 1)
     mx = np.concatenate((np.zeros([n, mx.shape[1]], dtype=np.int8), mx, np.zeros([n, mx.shape[1]], dtype=np.int8)), axis = 0)
     return mx
 
-def pic_dict():
-    return defaultdict(lambda: 0)
+def np_text_concat(ndarrays):
+    strings = [ndarray.astype(str) for ndarray in ndarrays]
+    result = strings[0]
+    for string in strings[1:]:
+        result =  np.core.defchararray.add(result, string)
+    return result
 
-def threegrid(d: Dict, i, j):
-    return [
-        d[(i-1,j-1)],
-        d[(i-1,j)],
-        d[(i-1,j+1)],
-        d[(i,j-1)],
-        d[(i,j)],
-        d[(i,j+1)],
-        d[(i+1,j-1)],
-        d[(i+1,j)],
-        d[(i+1,j+1)]
-        ]
+def pad_and_grid_concat(m, fill = 0):
+    padded = np.pad(m, 1, mode='constant', constant_values=fill)
 
-def step(start_pic, key):
-    pic = pic_dict()
-    coords = list(start_pic.keys())
-    for coord in coords:
-        print(coord)
-        x, y = coord
-        index_bin = ''.join(map(str, threegrid(start_pic, x, y)))
-        index = int(index_bin, 2)
-        val = key[index]
-        pic[(x,y)] = val
-        print(index_bin, index, val)
-    return pic
+
+
+def step(mx, map_fn, n = 1):
+    for _ in range(n):
+        padded = np.pad(mx, 2)
+        w, h = mx.shape
+        shifts = [padded[i:i+w+1, j:j+h+1] for i in range(1,4) for j in range(1,4)]
+        concat = np_text_concat(shifts)
+        mx = map_fn(concat)
+    return mx
+
+def to_string(mx: np.ndarray) -> str:
+    return '\n'.join([''.join(map(str, map(lambda x: "#" if x else ".", line))) for line in mx])
 
 def part1(data: str) -> int:
     key, start_pic = parse_input(data)
+    mapper = np.vectorize(lambda x: key[int(x, 2)])
     pic = start_pic
-    pic = step(pic, key)
-
+    pic = step(pic, mapper)
+    print(to_string(start_pic))
+    print('')
+    print(to_string(pic))
     # print(key)
     # print(pic)
     # print(append_zeros(pic, 2))
