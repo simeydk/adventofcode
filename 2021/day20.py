1778935dfbcf17f1da21f862af77e080f46e156d
Simey de Klerk <simeydeklerk@gmail.com>
2021-12-22 16:29:37 +0200

    day 20 part 2

--- 2021/day20.py
+++ 2021/day20.py
@@ -1,10 +1,11 @@
-from typing import DefaultDict, Dict, List
+from time import perf_counter
+from typing import List
 import numpy as np
-from collections import defaultdict
+from functools import lru_cache
 
 DAY = 20
 TEST_SOLUTION_1 = 35
-TEST_SOLUTION_2 = None
+TEST_SOLUTION_2 = 3351
 
 def read_file(filename) -> str:
     with open(filename, encoding="UTF-8") as f:
@@ -15,51 +16,48 @@ def hashperiod_to_binarray(s: str) -> List[int]:
 
 def parse_input(data: str):
     key_raw, pic_raw = data.split('\n\n')
-    key = hashperiod_to_binarray(key_raw)
-    pic = np.array([hashperiod_to_binarray(line) for line in pic_raw.splitlines()])
+    key = np.array(hashperiod_to_binarray(key_raw), dtype=np.int8)
+    pic = np.array([hashperiod_to_binarray(line) for line in pic_raw.splitlines()], dtype=np.int8)
     return key, pic
 
-def append_zeros(mx: np.ndarray, n: int = 1) -> np.ndarray:
-    mx = np.concatenate((np.zeros([mx.shape[0], n], dtype=np.int8), mx, np.zeros([mx.shape[0], n], dtype=np.int8)), axis = 1)
-    mx = np.concatenate((np.zeros([n, mx.shape[1]], dtype=np.int8), mx, np.zeros([n, mx.shape[1]], dtype=np.int8)), axis = 0)
-    return mx
+def shifts(mx: np.ndarray) -> np.ndarray:
+    x = mx.shape[0] - 2
+    y = mx.shape[1] - 2
+    return np.stack([mx[i:i+x, j:j+y] for i in range(3) for j in range(3)], axis = -1)
 
-def np_text_concat(ndarrays):
-    strings = [ndarray.astype(str) for ndarray in ndarrays]
-    result = strings[0]
-    for string in strings[1:]:
-        result =  np.core.defchararray.add(result, string)
+def pad(mx: np.ndarray, n = 1, value = 0) -> np.ndarray:
+    return np.pad(mx, n, mode="constant", constant_values = value)
+
+@lru_cache
+def reverse_binary_array(n = 9) -> np.ndarray:
+    return 2 ** np.arange(n-1,-1,-1)
+
+def bin_array_to_int(a: np.array) -> int:
+    return sum(a * reverse_binary_array(a.shape[0]))
+
+def step(mx: np.ndarray, key_function, n = 1) -> np.ndarray:
+    f = np.vectorize(key_function)
+    result = mx
+    for run in range(n):
+        constant_values = 1 if (key_function(1) == 1 and run % 2) else 0
+        padded = pad(result, 2, constant_values)
+        shifted = shifts(padded)
+        indexes = np.apply_along_axis(bin_array_to_int, -1, shifted)
+        result = f(indexes)
     return result
 
-def pad_and_grid_concat(m, fill = 0):
-    padded = np.pad(m, 1, mode='constant', constant_values=fill)
-
-
-
-def step(mx, map_fn, n = 1):
-    for _ in range(n):
-        padded = np.pad(mx, 2)
-        w, h = mx.shape
-        shifts = [padded[i:i+w, j:j+h] for i in range(1,4) for j in range(1,4)]
-        concat = np_text_concat(shifts)
-        mx = map_fn(concat)
-    return mx
-
 def to_string(mx: np.ndarray) -> str:
-    return '\n'.join([''.join(map(str, map(lambda x: "#" if x else ".", line))) for line in mx])
+    return '\n'.join([''.join(map(str, map(lambda x: "#" if x == 1 else "." if x == 0 else "?", line))) for line in mx])
 
-def part1(data: str) -> int:
+def part1(data: str, n = 2) -> int:
     key, start_pic = parse_input(data)
-    mapper = np.vectorize(lambda x: key[int(x, 2)])
-    pic = np.pad(start_pic, 5)
-    print(to_string(pic))
-    pic = step(pic, mapper, 2)
-    print('')
-    print(to_string(pic))
+    mapper = lambda x: key[x]
+    pic = start_pic
+    pic = step(start_pic, mapper, n)
     return np.sum(pic)
 
 def part2(data: str) -> int:
-    pass
+    return part1(data, 50)
 
 test_input = """..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
 
@@ -77,9 +75,10 @@ if TEST_SOLUTION_1:
     print(f"Solution 1:\n{part1(input_raw)}")
     if TEST_SOLUTION_2:
         assert part2(test_input) == TEST_SOLUTION_2
-        print(f"Solution 2:\n{part2(input_raw)}")
+        start = perf_counter()
+
+        print(f"Solution 2:\n{part2(input_raw)}  ({perf_counter() - start:.2f}s)")
     else:
         print(f"Test 2:\n{part2(test_input)}")
 else:
     print(f"Test 1:\n{part1(test_input)}")
-    
