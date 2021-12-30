6b1e9935cc893aeb90b167ff5a69860cae8a9355
Simey de Klerk <simeydeklerk@gmail.com>
2021-12-30 20:45:45 +0200

    day 23 wip

--- 2021/day23.py
+++ 2021/day23.py
@@ -1,7 +1,11 @@
-from typing import List, NamedTuple, Set
+import heapq
+import itertools
+from typing import Generator, List, NamedTuple, Optional, Set, Tuple
 from enum import Enum
 from itertools import combinations
 from dataclasses import dataclass, field
+from functools import cached_property, lru_cache
+from datetime import datetime
 
 DAY = 11
 TEST_SOLUTION_1 = None
@@ -11,16 +15,12 @@ def read_file(filename) -> str:
     with open(filename, encoding="UTF-8") as f:
         return f.read()
 
-class PodType(NamedTuple):
-    type: str
-    value: int
 
-PodTypes = {
-    'A': PodType('A', 1),
-    'B': PodType('B', 10),
-    'C': PodType('C', 100),
-    'D': PodType('D', 1000),
-}
+class PodType(Enum):
+    A = 1
+    B = 10
+    C = 100
+    D = 1000
 
 
 @dataclass(frozen = True)
@@ -28,72 +28,110 @@ class Position:
     x: int
     y: int
     connections: List['Position'] = field(default_factory=list, hash=False, repr=False)
-    contents: List['Pod'] = field(default_factory=list)
-
-    def __hash__(self):
-        return hash(self.__repr__())
-    
-
 
 class Pod(NamedTuple):
     type: PodType
-    position: Position
 
-    @property
-    def possible_moves(self) -> List['Position']:
-        return [p for p in self.position.connections if len(p.contents) == 0]
+Pair = Tuple[Pod, Position]
+@dataclass(frozen=True)
+class Burrow:
+    pairs:  Tuple[Pair, ...]
+    positions: List[Position] = field(default_factory=list, hash=False, repr=False)
+    _created: datetime = field(default_factory=datetime.now, hash=False, repr=False)
 
 
-class Burrow(NamedTuple):
-    positions: List[Position]
-    pods: List[Pod]
+    @cached_property
+    def occupied_positions(self) -> Set[Position]:
+        return {pos for _, pos in self.pairs}
 
-    @property
-    def possible_moves(self):
-        moves = []
-        for pod in self.pods:
-            moves.extend([pod, move for move in pod.possible_moves])
-        return moves
+    def possible_moves(self) -> Generator[Pair, None, None]:
+        for pod, position in self.pairs:
+            for connection in position.connections:
+                if connection not in self.occupied_positions:
+                    yield (pod, connection)
 
-    def move(pod: Pod, to: Position):
+    def move(self, pod: Pod, new_position: Position) -> 'Burrow':
+        pairs = tuple((the_pod, new_position if the_pod is pod else pos) for the_pod, pos in self.pairs)
+        return Burrow(pairs, self.positions)
+
+    def pod_at(self, position: Position) -> Optional[Pod]:
+        for pod, pos in self.pairs:
+            if pos == position:
+                return pod
+
+    def solve(self) -> Optional[Tuple[int, 'Burrow', List[Pair]]]:
+        queue: List[Tuple[int, int, Burrow, List[Pair]]] = []
+        completed: Set[Burrow] = set()
+        ticker = itertools.count()
+        tick = lambda: next(ticker)
+        heapq.heappush(queue, (0, tick(), self, []))
+        while queue:
+            cost, _, burrow, path = heapq.heappop(queue)
+            if burrow in completed: continue
+            if burrow == Burrow.solution: return cost, burrow, path
+            for move in burrow.possible_moves():
+                new_burrow = burrow.move(*move)
+                new_cost = move[0].type.value + cost
+                new_path = path + [move]
+                heapq.heappush(queue, (new_cost, tick(), new_burrow, new_path))
+            completed.add(burrow)
+        return None 
+
 
     def __str__(self):
         grid = [[' ' for _ in range(13)] for _ in range(5)]
         for pos in self.positions:
-            grid[pos.x][pos.y] =  pos.contents[0].type.type if pos.contents else "."
+            pod = self.pod_at(pos)
+            grid[pos.x][pos.y] =  pod.type.name if type(pod) == Pod else "."
+        return "\n".join(["".join(row) for row in grid])    
 
-        return "\n".join(["".join(row) for row in grid])
+    def __lt__(self, other):
+        return self._created < other._created
 
-    def __hash__(self):
-        return hash(self.__repr__())
+    @classmethod
+    def from_string(cls, data: str) -> 'Burrow':
+        positions = []
+        pairs: List[Tuple[Pod, Position]] = []
+        for i, line in enumerate(data.splitlines()):
+            for j, char in enumerate(line):
+                if char in ".ABCD":
+                    position = Position(i, j)
+                    positions.append(position)
+                    if char in "ABCD":
+                        pod = Pod(PodType[char])
+                        pairs.append((pod, position))
+        for pos_a, pos_b in combinations(positions, 2):
+            diff = (pos_a.x - pos_b.x, pos_a.y - pos_b.y)
+            if diff in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                pos_a.connections.append(pos_b)
+                pos_b.connections.append(pos_a)
+        return Burrow(tuple(pairs), positions)
 
 
+    @classmethod
+    @property
+    @lru_cache
+    def solution(cls) -> 'Burrow':
+        return Burrow.from_string("""#############
+#...........#
+###A#B#C#D###
+  #A#B#C#D#
+  #########""")
 
-def parse_input(data: str) -> Burrow:
-    positions = []
-    pods = []
-    for i, line in enumerate(data.splitlines()):
-        for j, char in enumerate(line):
-            if char in ".ABCD":
-                position = Position(i, j)
-                positions.append(position)
-                if char in "ABCD":
-                    pod = Pod(PodTypes[char], position)
-                    position.contents.append(pod)
-                    pods.append(pod)
-    for pos_a, pos_b in combinations(positions, 2):
-        diff = (pos_a.x - pos_b.x, pos_a.y - pos_b.y)
-        if diff in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-            pos_a.connections.append(pos_b)
-            pos_b.connections.append(pos_a)
-    return Burrow(positions, pods)
 
+solution_str = """#############
+#...........#
+###A#B#C#D###
+  #A#B#C#D#
+  #########"""
+
+assert Burrow.from_string(solution_str) == Burrow.solution
 
 def part1(data: str) -> int:
-    start = parse_input(data)
+    start = Burrow.from_string(data)
     print(str(start))
-    print(hash(start))
-    pass
+    print(start.occupied_positions)
+    print(start.solve())
 
 def part2(data: str) -> int:
     pass
@@ -102,6 +140,12 @@ test_input = """#############
 #...........#
 ###B#C#B#D###
   #A#D#C#A#
+  #########"""
+
+test_input = """#############
+#........D..#
+###A#B#C#.###
+  #A#B#C#D#
   #########"""
 
 input_raw = """#############
