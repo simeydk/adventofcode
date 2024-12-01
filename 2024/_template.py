4026d44ee4642201844a3bd10ee397b91be57044
Simey de Klerk <simeydeklerk@gmail.com>
2024-12-01 19:55:30 +0200

    add template for 2024

---
+++ 2024/_template.py
@@ -0,0 +1,38 @@
+def read_file_to_one_big_string(filename):
+    with open(filename) as f:
+        return f.read()
+
+
+day_number = 6
+part1_test_solution = None
+part2_test_solution = None
+test_input = """
+""".strip(
+    "\n"
+)
+
+
+input_raw = read_file_to_one_big_string(f"2022/data/day{day_number:02d}/input.txt")
+
+
+def part1(input_raw: str):
+    return 0
+
+
+def part2(input_raw: str):
+    return 0
+
+
+if part1_test_solution is None:
+    print(f"Part 1 Test: {part1(test_input)}")
+    quit()
+
+assert part1(test_input) == part1_test_solution
+print(f"Part 1: {part1(input_raw)}")
+
+if part2_test_solution is None:
+    print(f"Part 2 Test: {part2(test_input)}")
+    quit()
+
+assert part2(test_input) == part2_test_solution
+print(f"Part 2: {part2(input_raw)}")
