c8e7e5e3d2ee14096624f6b7f2bfe246feea7a56
Simey de Klerk <13088589+simeydk@users.noreply.github.com>
2024-12-07 10:42:21 +0000

    2024 day 3 inputs

--- 2024/_template.py
+++ 2024/_template.py
@@ -2,7 +2,7 @@ def read_file_to_one_big_string(filename):
     with open(filename) as f:
         return f.read()
 
-
+year = 2024
 day_number = 6
 part1_test_solution = None
 part2_test_solution = None
@@ -12,7 +12,7 @@ test_input = """
 )
 
 
-input_raw = read_file_to_one_big_string(f"2022/data/day{day_number:02d}/input.txt")
+input_raw = read_file_to_one_big_string(f"{year}/data/day{day_number:02d}/input.txt")
 
 
 def part1(input_raw: str):
