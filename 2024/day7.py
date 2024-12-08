ca0c24981e98fc460645a6aee122f816105a38c9
Simey de Klerk <simeydeklerk@gmail.com>
2024-12-08 23:43:18 +0200

    2024 day 7 solution

--- 2024/day7.py
+++ 2024/day7.py
@@ -1,26 +1,24 @@
-from concurrent.futures import ThreadPoolExecutor
-from functools import cmp_to_key
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path('')))
+from utils.runner import runner
+from utils.utils import read_file_to_string
+
 import itertools
-import re
-from collections import Counter
 from typing import Callable, Iterable, List, Set, Tuple, TypeVar
 
-def read_file_to_one_big_string(filename):
-    with open(filename) as f:
-        return f.read()
+
+
 
 year=2024
 day_number = 7
 part1_test_solution = 3749
 part2_test_solution = 11387
-test_input = """
-""".strip(
-    "\n"
-)
 
 
-test_input = read_file_to_one_big_string(f"{year}/data/day{day_number:02d}/test_input.txt")
-input_raw = read_file_to_one_big_string(f"{year}/data/day{day_number:02d}/input.txt")
+test_input = read_file_to_string(f"{year}/data/day{day_number:02d}/test_input.txt")
+input_raw = read_file_to_string(f"{year}/data/day{day_number:02d}/input.txt")
 
 def parse_line(line: str):
     result_str, num_str = line.split(": ")
@@ -88,18 +86,18 @@ def part2(input_raw: str):
 
 
 
+runner(part1, part2, test_input, input_raw, part1_test_solution, part2_test_solution)
 
+# if part1_test_solution is None:
+#     print(f"Part 1 Test: {part1(test_input)}")
+#     quit()
 
-if part1_test_solution is None:
-    print(f"Part 1 Test: {part1(test_input)}")
-    quit()
+# assert part1(test_input) == part1_test_solution
+# print(f"Part 1: {part1(input_raw)}")
 
-assert part1(test_input) == part1_test_solution
-print(f"Part 1: {part1(input_raw)}")
+# if part2_test_solution is None:
+#     print(f"Part 2 Test: {part2(test_input)}")
+#     quit()
 
-if part2_test_solution is None:
-    print(f"Part 2 Test: {part2(test_input)}")
-    quit()
-
-assert part2(test_input) == part2_test_solution
-print(f"Part 2: {part2(input_raw)}")
+# assert part2(test_input) == part2_test_solution
+# print(f"Part 2: {part2(input_raw)}")
